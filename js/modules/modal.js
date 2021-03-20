b99f1d97923506b5341e1e1b440a99aa61829bdd
João Pedro Vieira <jp_vieira@aol.com>
2021-03-20 15:03:09 -0300

    modal refatorado

--- js/modules/modal.js
+++ js/modules/modal.js
@@ -1,22 +1,47 @@
-export default function initModal() {
-  const botaoAbrir = document.querySelector('[data-modal="abrir"]');
-  const botaoFechar = document.querySelector('[data-modal="fechar"]');
-  const containerModal = document.querySelector('[data-modal="container"]');
+export default class Modal {
+  constructor(open, close, container) {
+    this.open = document.querySelector('[data-modal="abrir"]');
+    this.close = document.querySelector('[data-modal="fechar"]');
+    this.container = document.querySelector('[data-modal="container"]');
 
-  function toggleModal(event) {
+
+    // bind this ao callback para
+    // fazer referência ao objeto
+    // da classe
+
+    this.eventToggleModal = this.eventToggleModal.bind();
+    this.cliqueForaModal = this.cliqueForaModal.bind();
+  };
+
+  // abre ou fecha ou modal
+  toggleModal() {
+    this.container.classList.toggle('ativo');
+  };
+
+  // adiciona o evento de toggle ao modal
+  eventToggleModal(event) {
     event.preventDefault();
-    containerModal.classList.toggle('ativo');
+    this.toggleModal();
+  };
+
+  // fecha o modal ao clicar no lado de fora
+  cliqueForaModal(event) {
+    if (event.target === this.container) {
+      this.toggleModal();
+    };
+  };
+
+  // adiciona os eventos aos elementos do modal
+  addModalEvent() {
+    this.open.addEventListener('click', this.eventToggleModal);
+    this.close.addEventListener('click', this.eventToggleModal);
+    this.container.addEventListener('click', this.cliqueForaModal);
   }
 
-  function cliqueForaModal(event) {
-    if (event.target === this) {
-      toggleModal(event);
+  init() {
+    if (this.open && this.close && this.container) {
+      this.addModalEvent();
     }
-  }
-
-  if (botaoAbrir && botaoFechar && containerModal) {
-    botaoAbrir.addEventListener('click', toggleModal);
-    botaoFechar.addEventListener('click', toggleModal);
-    containerModal.addEventListener('click', cliqueForaModal);
+    return this;
   }
 }
